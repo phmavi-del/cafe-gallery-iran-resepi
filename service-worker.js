64af8c3df67cda6ca7729368c264a1263b48e71b
phmavi-del <ph.mavi@gmail.com>
2026-07-03 17:20:54 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,45 @@
-
-const CACHE_NAME = 'cafe-gallery-v1';
+const CACHE_NAME = "cafe-gallery-v1";
 
 const urlsToCache = [
-'/',
-'index.html',
-'dashboard_pro.html',
-'recipes.html',
-'inventory.html',
-'notes.html',
-'contacts.html',
-'calculator.html',
-'timer.html',
-'backup.html',
-'settings.html',
-'manifest.json'
+  "./",
+  "./index.html",
+  "./dashboard_pro.html",
+  "./recipes.html",
+  "./inventory.html",
+  "./contacts.html",
+  "./notes.html",
+  "./settings.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener('install', event => {
- event.waitUntil(
-   caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
- );
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', event => {
- event.respondWith(
-   caches.match(event.request).then(response => {
-     return response || fetch(event.request);
-   })
- );
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
